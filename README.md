85f5fa930461ad867189c62b3e9174f654c96be6
ridermansb <ridermansb@gmail.com>
2019-03-19 20:58:52 -0300

    Add basic infos

---
+++ README.md
@@ -0,0 +1,13 @@
+It's me, @ridermansb!
+
+> based in [@bitandbang](https://github.com/bnb/bitandbang) work and [@gocastilla](https://github.com/gocastilla/gocastilla)
+
+# Usage
+
+Via npx:
+
+```
+npx ridermansb
+```
+
+Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
---
+++ src/index.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+
+clear();
+
+var prompt = inquirer.createPromptModule();
+
+const questions = [
+  {
+    type: "list",
+    name: "action",
+    message: "What you want to do?",
+    choices: [
+      {
+        name: `Send me an ${chalk.green.bold("email")}?`,
+        value: () => {
+          open("mailto:ridermansb@gmail.com");
+          console.log("\nDone, see you soon.\n");
+        }
+      },
+      {
+        name: "Just quit.",
+        value: () => {
+          console.log("Ok, bye.\n");
+        }
+      }
+    ]
+  }
+];
+
+const data = {
+  name: chalk.bold.green("        Riderman de Sousa Barbosa"),
+  handle: chalk.white("@ridermansb"),
+  work: `${chalk.white("Senior Font-end Developer at")} ${chalk
+    .hex("#2b82b2")
+    .bold("MaxMilhas")}`,
+  blog: chalk.gray("https://medium.com/") + chalk.whiteBright("@ridermansb"),
+  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("ridermansb"),
+  npm: chalk.gray("https://npmjs.com/") + chalk.red("~ridermansb"),
+  github: chalk.gray("https://github.com/") + chalk.green("ridermansb"),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("ridermansb"),
+  web: chalk.cyan("https://about.me/ridermansb"),
+  npx: chalk.red("npx") + " " + chalk.white("ridermansb"),
+
+  labelWork: chalk.white.bold("       Work:"),
+  labelBlog: chalk.white.bold("     Medium:"),
+  labelTwitter: chalk.white.bold("    Twitter:"),
+  labelnpm: chalk.white.bold("        npm:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+  labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+  [
+    `${data.name} / ${data.handle}`,
+    ``,
+    `${data.labelWork}  ${data.work}`,
+    `${data.labelBlog}  ${data.blog}`,
+    `${data.labelTwitter}  ${data.twitter}`,
+    `${data.labelnpm}  ${data.npm}`,
+    `${data.labelGitHub}  ${data.github}`,
+    `${data.labelLinkedIn}  ${data.linkedin}`,
+    `${data.labelWeb}  ${data.web}`,
+    ``,
+    `${data.labelCard}  ${data.npx}`,
+    ``,
+    `${chalk.italic(
+      "I'm curious, enthusiastic and student most of the time."
+    )}`,
+    `${chalk.italic("The rest of the time I write code that others can read.")}`
+  ].join("\n"),
+  {
+    margin: 1,
+    float: 'center',
+    padding: 1,
+    borderStyle: "single",
+    borderColor: "green"
+  }
+);
+
+console.log(me);
+const tip = [
+  `Tip: Try ${chalk.cyanBright.bold(
+    "cmd/ctrl + click"
+  )} on the links above`,
+  '',
+].join("\n");
+console.log(tip);
+
+
+prompt(questions).then(answer => answer.action());
